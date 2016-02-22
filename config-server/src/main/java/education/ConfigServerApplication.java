f3bd8bfce6661c202d2c9ebfbca26247773866a3
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2016-02-22 22:54:35 +0200

    config server

--- config-server/src/main/java/education/ConfigServerApplication.java
+++ config-server/src/main/java/education/ConfigServerApplication.java
@@ -6,10 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
